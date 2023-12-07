884be147c3cec4c8dac07afc8e0166c9894bf463
eunjinjeong <eunjin.jeong.5115@gmail.com>
2023-12-07 10:14:57 +0900

    11-3 수정

--- lib/1205/12-4/tangible_asset.dart
+++ lib/1205/12-4/tangible_asset.dart
@@ -20,11 +20,27 @@ abstract class TangibleAsset extends Asset implements Thing {
   set weight(double value) {
     _weight = value;
   }
-
 }
 
-// TangibleAsset는 Asset 클래스를 상속하고, Thing 인터페이스를 구현한다.
-// 기본적인 자산 속성과 동작을 확장하고, 인터페이스를 통해 특정 동작을 강제하는 것!
+/* TangibleAsset은 Asset 클래스를 상속하고, Thing 인터페이스를 구현한다.
+   Asset 속성의 동작을 확장 + 인터페이스를 통해 특정 동작을 강제!
+   Thing 인터페이스의 weight 속성을 구현 + 값을 캡슐화하여 외부에서 안전하게 접근하고 조작
+ */
 
-//Thing 인터페이스의 weight 속성을 구현하고 값을 캡슐화하여 외부에서 안전하게 접근하고 조작
-// => weight 속성과 일관된 동작을 가지게 됨.
+/* 아래 코드도 가능한데, getter/setter 없이 직접 변수를 선언함.
+ 더 깔끔하기도 하고, interface에는 변수를 둘 수 있지만 두지 않는다.
+
+abstract class TangibleAsset extends Asset implements Thing {
+String color;
+
+  @override
+  double weight;
+
+  TangibleAsset(
+     this.weight, {
+     required super.name,
+     required super.price,
+     required this.color,
+      });
+}
+ */
